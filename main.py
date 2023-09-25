54ef217b4a925915e6a82d6929a609c0f71b34e9
Og4rek <piotrlyczkopl@gmail.com>
2023-09-25 21:17:18 +0200

    Small fixes for models from Attentive Group Equivariant Convolutional Networks paper.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.dataset import Dataset
-from src.model import Model
 from src.pcammodel import PCAMModel
+from att_gconvs.experiments.pcam.models.densenet import *
 
 
 def main():
@@ -9,10 +9,15 @@ def main():
     batch_size = 64
 
     data_pcam = Dataset(root=dataset_folder, batch_size=batch_size)
-    model = Model()
+
+    model = P4DenseNet(n_channels=13)
+    model(torch.rand([1, 3, 96, 96]))
+
     pcam_model = PCAMModel(dataset=data_pcam, model=model, batch_size=batch_size, output_directory=output_folder)
 
     print(pcam_model.model)
+    print("Model parameters: ", end='')
+    num_params(model)
 
     print('\nTraining: ')
     pcam_model.train()
